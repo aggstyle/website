0d3efbb3caa1f280b8abb93e285756f466442e55
tripeh <squall@outlook.com>
2015-12-06 13:17:48 -0200

    iniciando o bower e o gulp

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+'use strict';
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+var reload = browserSync.reload;
+
+
+// Save a reference to the `reload` method
+
+// Watch scss AND html files, doing different things with each.
+gulp.task('serve', function() {
+
+    // Serve files from the root of this project
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+
+    gulp.watch("*.html").on("change", browserSync.reload);
+    gulp.watch('*.js').on("change", browserSync.reload);
+
+});
